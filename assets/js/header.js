203bbdeecc0b347c809ac228e7f515c69e13aabb
Lucas Bulgarelli <lucas.bulgarelli@hotmail.com>
2020-09-04 23:58:32 -0300

    add header, experience and projects

---
+++ assets/js/header.js
@@ -0,0 +1,32 @@
+me_height = $("#photo").outerHeight();
+social_height = $(".social").outerHeight();
+me_margin = ($("#photo").outerHeight(true) - me_height) / 2;
+
+$(window).on('resize', function(){
+    $(window).scrollTop(0);
+    $("#photo").outerHeight('');
+    $(".social").outerHeight('');
+    $("#photo").css("margin", '');
+    me_height = $("#photo").outerHeight();
+    social_height = $(".social").outerHeight();
+    me_margin = ($("#photo").outerHeight(true) - me_height) / 2;
+});
+
+$(window).scroll(function(e) {
+    scroll_top = $(window).scrollTop();
+    // Disable resizing for small screens and negative scroll
+    if(scroll_top < 0 || $(window).width() <= 768) return;
+    // Resize photo
+    new_height = me_height - scroll_top;
+    $("#photo").outerHeight(new_height);
+    // Resize photo margins for better proportions
+    if(scroll_top <= 250)
+        $("#photo").css("margin", me_margin * (1 - scroll_top / 350));
+    else
+        $("#photo").css("margin", me_margin * (1 - 250 / 350));
+    // Disable resizing social media for small screens
+    if($(window).width() <= 945) return;
+    // Resize social media icons
+    $(".social").outerHeight(social_height - scroll_top / 5);
+    console.log(scroll_top);
+});
